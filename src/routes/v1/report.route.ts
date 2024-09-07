92655e76bc16a9f337a814ced053d97476d6a27e
Elad Yefet <yefet.elad@gmail.com>
2024-09-08 00:10:29 +0300

    init ses

--- src/routes/v1/report.route.ts
+++ src/routes/v1/report.route.ts
@@ -1,14 +1,14 @@
 import { Request, Response, Router } from 'express';
-import { summarizePDFDownload, summarizePDFEMail, generateView } from "../../controllers/report";
+import { reportPDFDownload, reportPDFEMail, reportView } from "../../controllers/report";
 
 const router = Router();
 
-router.get("/pdf/download", summarizePDFDownload);
-router.get("/pdf/email", summarizePDFEMail);
-router.get("/html/view", generateView);
+router.get("/pdf/download", reportPDFDownload);
+router.get("/pdf/email", reportPDFEMail);
+router.get("/html/view", reportView);
 
-router.post("/pdf/download", summarizePDFDownload);
-router.post("/pdf/email", summarizePDFEMail);
-router.post("/html/view", generateView);
+router.post("/pdf/download", reportPDFDownload);
+router.post("/pdf/email", reportPDFEMail);
+router.post("/html/view", reportView);
 
 export default router;
