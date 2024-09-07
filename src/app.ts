036b92efda89e630f8950864e1a80fd438d3b00c
Elad Yefet <yefet.elad@gmail.com>
2024-09-07 17:25:08 +0300

    user info log

--- src/app.ts
+++ src/app.ts
@@ -74,6 +74,7 @@ app.get('/helloworld', (req: any, res: any) => {
     console.log("=========================================================================")
     console.log("Hello World Route Event payload: ", req); // Logs the full event payload
     console.log("=========================================================================")
+    console.log("xxx my raw event" + req.rawHeaders)
     res.json({ "Route": "Called from Hello World route" });
 });
 
